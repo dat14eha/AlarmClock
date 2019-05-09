ce123e404925f9497768dba895d93e251bf45ccc
dat14eha <dat14eha@student.lu.se>
2019-05-09 13:56:25 +0200

    d

--- app/src/main/java/com/example/alarmclock/RingingActivity.java
+++ app/src/main/java/com/example/alarmclock/RingingActivity.java
@@ -4,18 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 
-import androidx.legacy.content.WakefulBroadcastReceiver;
+import com.ncorti.slidetoact.SlideToActView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class RingingActivity extends AppCompatActivity {
     private Button fish;
@@ -25,22 +23,24 @@ public class RingingActivity extends AppCompatActivity {
     public static RingingActivity instance() {
         return inst;
     }
-
+    SlideToActView slideToActView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_ringing);
 
-        fish = (Button)findViewById(R.id.ShutDown);
-
-        fish.setOnClickListener(new View.OnClickListener() {
+        slideToActView = (SlideToActView)findViewById(R.id.slide_to_unlock);
+        slideToActView.setOnSlideCompleteListener(new SlideToActView.OnSlideCompleteListener() {
             @Override
-            public void onClick(View v) {
+            public void onSlideComplete(@NotNull SlideToActView slideToActView) {
                 openFish();
             }
         });
         play();
         mp.setLooping(true);
+
+        //RotateAnimation rotate= (RotateAnimation) AnimationUtils.loadAnimation(this,R.anim.rotateAnimation);
+        //slideToActView.setAnimation(rotate);
     }
 
     @Override
