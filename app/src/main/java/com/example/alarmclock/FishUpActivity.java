729ff9d966e23ecca7ae15482dcccd06b8c77b56
dat14eha <dat14eha@student.lu.se>
2019-05-17 15:07:29 +0200

    ö

--- app/src/main/java/com/example/alarmclock/FishUpActivity.java
+++ app/src/main/java/com/example/alarmclock/FishUpActivity.java
@@ -2,15 +2,45 @@ package com.example.alarmclock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 
-public class FishUpActivity extends AppCompatActivity {
+public class FishUpActivity extends AppCompatActivity implements SensorEventListener {
+    private Sensor mySensor;
+    private SensorManager SM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fish_up);
+
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        // Accelerometer Sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Register sensor Listener
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void vibrate(){
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(200);
+    }
 }
